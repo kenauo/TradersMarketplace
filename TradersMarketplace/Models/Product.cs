aad1ab45192eb1e5789603dca61b1630d0e0d70f
kenauo <kenneth.cauchi9@gmail.com>
2015-01-05 04:32:20 +0100

    Products
    Added the products functionality.

---
+++ TradersMarketplace/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System.Data.Entity;
+
+namespace TradersMarketplace.Models
+{
+    public class Product
+    {
+        public int ID { get; set; }
+        public int SellerID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+    }
+
+    public class ProductDBContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+    }
+}
