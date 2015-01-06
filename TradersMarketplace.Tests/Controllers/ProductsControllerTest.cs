b8157e4267f39153cab6d97a80a7ded7a7ecd04a
kenauo <kenneth.cauchi9@gmail.com>
2015-01-06 06:55:50 +0100

    Added Unit Test Stubs

---
+++ TradersMarketplace.Tests/Controllers/ProductsControllerTest.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TradersMarketplace.Tests.Controllers
+{
+    [TestClass]
+    class ProductsControllerTest
+    {
+        [TestMethod]
+        public void Create()
+        {
+            //To implement later
+        }
+
+        [TestMethod]
+        public void Read()
+        {
+            //To implement later
+        }
+
+        [TestMethod]
+        public void Update()
+        {
+            //To implement later
+        }
+
+        [TestMethod]
+        public void Delete()
+        {
+            //To implement later
+        }
+    }
+}
